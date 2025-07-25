a03cdda4d41bae01f7f5ce81d3abe4bc8006dcdc
Muzaffar Ali <muzafark834@gmail.com>
2025-07-25 11:29:51 +0500

    Assignemt done

--- script.js
+++ script.js
@@ -73,17 +73,32 @@ function Assign_5(){
    console.log(`You are from ${city}`);
 } 
 
+
 function Assign_6(){
     console.log("6.If Statement and Comparison Operators: Controlling Flow");
     let user_age = Number(prompt("Enter your age"))
     if(user_age <30){
         console.log("You are still young man");
     }
+
     let user_num = Number(prompt("Enter any number: "))
-    if(user_num == 7){
+    if(user_num %2 == 0){
         console.log("Even number");
+    }else{
+        console.log("old number");
     }
-    
+
+    let num1 = 10;
+    let num2 = 20;
+    if(num1 > num2){
+        console.log(`${num1} is greater then ${num2}`); 
+    }
+    else if(num2 > num1){
+        console.log(`${num2} is greater then ${num1}`);
+    }else{
+        console.log(` ${num1} == ${num2}`);  
+    }
 }
+
 
 
